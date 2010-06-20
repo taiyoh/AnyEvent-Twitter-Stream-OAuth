9f2c90edbc50cf94d59876b9af11e5ea74f427fa
Taiyoh Tanaka <sun.basix@gmail.com>
2010-06-20 21:22:16 +0900

    initial commit

---
+++ lib/AnyEvent/Twitter/Stream/OAuth.pm
@@ -0,0 +1,33 @@
+package AnyEvent::Twitter::Stream::OAuth;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::Twitter::Stream::OAuth -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::Twitter::Stream::OAuth;
+
+=head1 DESCRIPTION
+
+AnyEvent::Twitter::Stream::OAuth is
+
+=head1 AUTHOR
+
+Taiyoh Tanaka E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
